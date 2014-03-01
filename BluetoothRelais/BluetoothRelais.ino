134b0e70ffa5e64ef3b6ce3b1d7b526ff20b585e
Dieter Schoen <dieter@schoen.or.at>
2014-03-01 22:58:05 +0100

    BT and I2C from ArduinoBT to Robot working

--- BluetoothRelais/BluetoothRelais.ino
+++ BluetoothRelais/BluetoothRelais.ino
@@ -1,50 +1,45 @@
 //Arduino BT V06 w/ Atmega168
 //on MAC: dev.tty.ARDUINOBT-BluetoothSerial
 
-#include <SoftwareSerial.h>
+#include <Wire.h>
 
-SoftwareSerial robotSerial(10,11);
 
 uint8_t v1; // variable to receive data from the serial port
 uint8_t v2; // variable to receive data from the serial port
 
 int ledpin = 13; // LED connected to pin 48 (on-board LED)
 
-uint8_t inBuf[6];
+uint8_t btBuf[6];
+uint8_t i2cBuf[6];
 
 int idx;
 
 void setup() {
-  pinMode(ledpin, OUTPUT);  // pin 48 (on-board LED) as OUTPUT
+  //pinMode(ledpin, OUTPUT);  // pin 48 (on-board LED) as OUTPUT
   Serial.begin(115200);
 
-  robotSerial.begin(76800);
+  Wire.begin(2);
+  Wire.onRequest(requestEvent);
+  Wire.onReceive(receiveEvent);
   v1 = 0;
   v2 = 0;
   idx = 6;
+  btBuf[5] = 0x88;
 }
 
 void loop() {
+  //Serial.write(".\n");
+  
   if(Serial.available())
   {
     v2 = v1;
     v1 = Serial.read();
     if(idx < 6)
     {
-      inBuf[idx++] = v1;
+      btBuf[idx++] = v1;
       if(idx==6)
       {
-        robotSerial.write(inBuf,6);
-        if(robotSerial.available())
-        {
-          inBuf[0] = robotSerial.read();
-          inBuf[1] = robotSerial.read();
-          inBuf[2] = robotSerial.read();
-          inBuf[3] = robotSerial.read();
-          inBuf[4] = robotSerial.read();
-          inBuf[5] = robotSerial.read();
-         }
-        Serial.write(inBuf,6);
+        Serial.write(i2cBuf,6);
       }
     }
     else if(v1 == 0xFF && v2 == 0xFF)
@@ -53,4 +48,23 @@ void loop() {
       idx = 0;      
     }
   }
+  
+  //delay(200);
 }
+
+void requestEvent()
+{
+  //Serial.write("reqE\n");
+  Wire.write(btBuf,6);
+}
+
+void receiveEvent(int howMany)
+{
+  Serial.write("recE\n");
+  int i = 0;
+  while(Wire.available()) // loop through all but the last
+  {
+          i2cBuf[i++] = Wire.read();
+  }
+}
+
--- RobotController/RobotController.ino
+++ RobotController/RobotController.ino
@@ -1,29 +1,30 @@
-#include <AltSoftSerial.h>
-
 //Arduino Robot Control
 //on MAC: /dev/tty.usbmodem641
 
 #include <ArduinoRobot.h>
+#include <Wire.h>
+#include <SPI.h>
 #include <utility/RobotTextManager.h> 
 
-AltSoftSerial robotSerial(3,4);
-
 uint8_t inBuf[6];
-char textBuf[20];
 
-const int p3 = TKD3;
-const int p4 = TKD4;
-const int p5 = TKD5;
+char textBuf1[30];
+char textBuf2[30];
+char textBuf3[30];
 
-int pin = TKD5;
+
+int line = 0;
+int16_t left, right;
+
+int i=0; 
 
 void setup() {
+  Wire.begin();
   Robot.begin();
   Robot.beginTFT();
-  robotSerial.begin(76800);
+  //robotSerial.begin(76800);
   textManager.setMargin(5,5);
   textManager.writeText(0,0,"HEL.LO");
-
 }
 
 void loop() 
@@ -41,20 +42,34 @@ void loop()
 */
   //textManager.writeText(3,4,"HOLLA!");
   //delay(300);
+  i=0;
+  Wire.requestFrom(2,6);
+  while(Wire.available())
+  {
+    inBuf[i++] = Wire.read();
+  }
+  
+  left  = inBuf[1] ? inBuf[2] - 256 : inBuf[2];
+  right = inBuf[3] ? inBuf[4] - 256 : inBuf[4];
+ 
+ 
+  sprintf(textBuf1, "%d:%02x %02x%02x %02x%02x %02x", i,
+    inBuf[0], inBuf[1], inBuf[2], inBuf[3], inBuf[4], inBuf[5]);
+  sprintf(textBuf2, "left :%4d %d", left, inBuf[2]);
+  sprintf(textBuf3, "right:%4d %d", right,inBuf[4]);
+  
+  
+  Robot.motorsWrite(left, right);
+    
+  Robot.clearScreen();
+  
+  textManager.writeText(1,0,textBuf1);
+  textManager.writeText(2,0,textBuf2);
+  textManager.writeText(3,0,textBuf3);
 
-  if(robotSerial.available())
-    {
-      inBuf[0] = robotSerial.read();
-      inBuf[1] = robotSerial.read();
-      inBuf[2] = robotSerial.read();
-      inBuf[3] = robotSerial.read();
-      inBuf[4] = robotSerial.read();
-      inBuf[5] = robotSerial.read();
-      Robot.clearScreen();
-      sprintf(textBuf, "SSC:%u", inBuf[0]);
-      textManager.writeText(1,0,textBuf);
-      inBuf[5] = 0x99;
-      robotSerial.write(inBuf,6);
-    }
-
+  //inBuf[5] = 0x99;
+  //Wire.beginTransmission(2);
+  //Wire.write(inBuf,6);
+  //Wire.endTransmission();
+  delay(918);
 }
